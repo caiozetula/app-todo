ed319f60ec6151c787722aa53835029d74cc1e1c
caiozetula <caiozetula2@gmail.com>
2021-09-27 18:43:45 -0300

    alteração na interface

--- ToDo/src/app/home/home.page.ts
+++ ToDo/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
-import { ActionSheetController, AlertController, LoadingController } from '@ionic/angular';
+import { ActionSheetController, AlertController, LoadingController, ModalController } from '@ionic/angular';
+import { ModalpagePage } from '../modalpage/modalpage.page';
 import { UtilService } from '../services/util.service';
 
 @Component({
@@ -10,62 +11,69 @@ import { UtilService } from '../services/util.service';
 })
 export class HomePage {
   tasks: any[] = [];
-  constructor(private alertCtrl: AlertController, private utilServiceCtrl: UtilService, private actionSheetCtrl: ActionSheetController, private loadingCtrl: LoadingController) {
+  numHoje: number;
+  constructor(private alertCtrl: AlertController, 
+    private utilServiceCtrl: UtilService, 
+    private actionSheetCtrl: ActionSheetController, 
+    private loadingCtrl: LoadingController,
+    private modalCtrl: ModalController) {
     let taskJson = localStorage.getItem('taskDb');
+    let numJson = localStorage.getItem('numHoje');
+    this.numHoje = 0;
     if (taskJson != null) {
       this.tasks = JSON.parse(taskJson);
+      this.numHoje = Number.parseInt(numJson);
     }
   }
 
-  async add(newTask: string) {
-    //valida se o usuário preencheu a tarefa
-    if (newTask.trim().length < 1) {
-      this.utilServiceCtrl.showToast('Informe o que deseja fazer!');
-      return;
-    }
+  async add(newTask: any) {
     let loading = this.loadingCtrl.create({ message: 'Processando' });
     (await loading).present();
-    let task = { name: newTask, done: false };
+    let task = { name: newTask.name, done: false, hoje: newTask.hoje};
     this.tasks.push(task);
-    this.updateLocalStorage();
     (await loading).dismiss();
+    if(task.hoje == true){
+      this.numHoje++;
+    }
+    this.updateLocalStorage();
     this.utilServiceCtrl.showToast('Adicionado com sucesso!');
   }
 
   updateLocalStorage() {
     localStorage.setItem('taskDb', JSON.stringify(this.tasks));
+    localStorage.setItem('numHoje', this.numHoje.toString());
   }
 
-  async showAdd() {
-    const alert = await this.alertCtrl.create({
-      header: 'Adicionar tarefa',
-      inputs: [
-        {
-          name: 'newTask',
-          type: 'text',
-          placeholder: 'Alguma tarefa para a lista'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancelar',
-          role: 'cancel',
-          cssClass: 'secondary',
-          handler: () => {
-            console.log('clicked cancel');
-          }
-        },
-        {
-          text: 'Adicionar',
-          cssClass: 'secondary',
-          handler: (form) => {
-            this.add(form.newTask);
-          }
-        },
-      ]
-    });
-    await alert.present();
-  }
+  // async showAdd() {
+  //   const alert = await this.alertCtrl.create({
+  //     header: 'Adicionar tarefa',
+  //     inputs: [
+  //       {
+  //         name: 'newTask',
+  //         type: 'text',
+  //         placeholder: 'Alguma tarefa para a lista'
+  //       },
+  //     ],
+  //     buttons: [
+  //       {
+  //         text: 'Cancelar',
+  //         role: 'cancel',
+  //         cssClass: 'secondary',
+  //         handler: () => {
+  //           console.log('clicked cancel');
+  //         }
+  //       },
+  //       {
+  //         text: 'Adicionar',
+  //         cssClass: 'secondary',
+  //         handler: (form) => {
+  //           this.add(form.newTask);
+  //         }
+  //       },
+  //     ]
+  //   });
+  //   await alert.present();
+  // }
 
   async openActions(task: any) {
     const actionSheet = await this.actionSheetCtrl.create({
@@ -75,6 +83,19 @@ export class HomePage {
         icon: task.done ? 'radio-button-off' : 'checkmark-circle',
         handler: () => {
           task.done = !task.done;
+          this.updateLocalStorage();
+        }
+      },
+      {
+        text: task.hoje ? 'Não é para hoje' : 'Para hoje',
+        icon: task.hoje ? 'bookmark-outline' : 'bookmark',
+        handler: () => {
+          task.hoje = !task.hoje;
+          if(task.hoje == true){
+            this.numHoje++;
+          }else{
+            this.numHoje--;
+          }
           this.updateLocalStorage();
         }
       },
@@ -109,6 +130,9 @@ export class HomePage {
           handler: () => {
             loading.present();
             this.tasks = this.tasks.filter(taskArray => task != taskArray);
+            if(task.hoje == true){
+              this.numHoje--;
+            }
             this.updateLocalStorage();
             loading.dismiss();
             this.utilServiceCtrl.showToast('Removido com sucesso!');
@@ -119,4 +143,16 @@ export class HomePage {
     alert.present();
   }
 
+  async showAdd2(){
+    const modal = await this.modalCtrl.create({
+      component: ModalpagePage,
+    });
+    await modal.present();
+    const { data } = await modal.onWillDismiss();
+    //console.log(data);
+    if(data != undefined && data != null){
+      this.add(data);
+    }
+    return;
+  }
 }
